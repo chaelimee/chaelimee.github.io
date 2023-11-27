7d1722a47f8ad7e6bc6f9bb34338c7befe4a9689
chaelimee <bincl418@naver.com>
2023-11-27 20:28:06 +0900

    테마 수정

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1701083935';
+const cacheName = 'chirpy-1701084462';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
