acd8c32707d35fc0b2526cc84fa1fa3686b5cd74
chaelimee <bincl418@naver.com>
2023-11-27 20:40:32 +0900

    .gitignore 파일 생성

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1701084941';
+const cacheName = 'chirpy-1701085213';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
